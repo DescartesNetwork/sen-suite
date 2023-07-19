d04f8e84a7f91a85647dc80689c51604bc0a2563
tuphan-dn <tuphan@descartes.network>
2023-07-19 15:15:52 +0700

    bugfix

--- src/app/token-creation/edit-token/[tokenAddress]/page.tsx
+++ src/app/token-creation/edit-token/[tokenAddress]/page.tsx
@@ -5,6 +5,7 @@ import { useEffect } from 'react'
 import { TokenLogo, TokenName, TokenSymbol } from '@/components/token'
 
 import { isAddress } from '@/helpers/utils'
+import { ArrowLeft, Settings } from 'lucide-react'
 
 export default function EditSpecificToken() {
   const { tokenAddress } = useParams()
@@ -16,7 +17,18 @@ export default function EditSpecificToken() {
 
   return (
     <div className="grid grid-cols-12 gap-2">
-      <div className="col-span-full">Edit Specific Token</div>
+      <div className="col-span-full flex flex-row gap-2 items-center justify-between">
+        <button
+          className="btn btn-circle btn-ghost btn-sm"
+          onClick={() => router.push('/token-creation/edit-token')}
+        >
+          <ArrowLeft className="h-4 w-4" />
+        </button>
+        <p className="font-bold">Token Metadata</p>
+        <button className="btn btn-circle btn-ghost btn-sm">
+          <Settings className="h-4 w-4" />
+        </button>
+      </div>
       <div className="col-span-full flex flex-row gap-2 items-center">
         <TokenLogo tokenAddress={tokenAddress} />
         <p className="font-bold">
