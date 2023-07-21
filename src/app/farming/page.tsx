42106f93fe867b4ee57f15b6543d25062720baf4
tuphan-dn <tuphan@descartes.network>
2023-07-21 09:43:56 +0700

    farming panel

--- src/app/farming/page.tsx
+++ src/app/farming/page.tsx
@@ -1,27 +1,12 @@
 'use client'
 
+import FarmingPanel from './panel'
+
 export default function Farming() {
   return (
     <div className="grid grid-cols-12 gap-x-2 gap-y-4">
-      <div className="col-span-full card w-full shadow-lg p-4 ring-1 ring-base-100 bg-gradient-to-br from-teal-200 to-lime-200 flex flex-row">
-        <div className="flex-auto flex flex-col gap-8">
-          <div className="flex flex-row gap-2">
-            <h4>Sen Farming</h4>
-            <h5 className="opacity-60">v2</h5>
-          </div>
-          <div className="flex flex-row gap-2">
-            <div className="">
-              <p>Total Value Locked</p>
-              <h5>$2,053.38</h5>
-            </div>
-            <span className="divider divider-horizontal m-0" />
-            <div className="">
-              <p>Your Reward</p>
-              <h5>$2,053.38</h5>
-            </div>
-          </div>
-        </div>
-        <div>Image</div>
+      <div className="col-span-full">
+        <FarmingPanel />
       </div>
     </div>
   )
---
+++ src/app/farming/panel.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+export default function FarmingPanel() {
+  return (
+    <div className="card w-full shadow-lg p-8 ring-1 ring-base-100 bg-gradient-to-br from-lime-200 to-teal-300 flex flex-row-reverse flex-wrap gap-x-2 gap-y-16 justify-center">
+      <div className="w-48 relative -mb-4 -mr-4">
+        <img
+          className="w-full"
+          src="/farming-illustration.png"
+          alt="farming-illustration"
+        />
+        <img
+          className="absolute -top-6 left-4 animate-bounce"
+          src="/farming-coin-1.svg"
+          alt="farming-coin-1"
+        />
+        <img
+          className="absolute -top-12 right-2 animate-bounce"
+          src="/farming-coin-2.svg"
+          alt="farming-coin-2"
+        />
+      </div>
+      <div className="flex-auto flex flex-col gap-8 text-slate-800">
+        <div className="flex flex-row gap-2">
+          <h4>Sen Farming</h4>
+          <h5 className="opacity-60">v2</h5>
+        </div>
+        <div className="flex flex-row gap-2">
+          <div className="">
+            <p className="text-sm">TVL</p>
+            <h5>$2,053.38</h5>
+          </div>
+          <span className="divider divider-horizontal m-0" />
+          <div className="">
+            <p className="text-sm">Your Reward</p>
+            <h5>$2,053.38</h5>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
