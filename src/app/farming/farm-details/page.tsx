5ef4823e096394631a5c9f19894b1dc2e74cf68d
tuphan-dn <tuphan@descartes.network>
2023-07-23 22:14:21 +0700

    relayout

--- src/app/farming/farm-details/page.tsx
+++ src/app/farming/farm-details/page.tsx
@@ -65,7 +65,7 @@ export default function FarmDetails() {
         <div className="col-span-full @xl/left:col-span-6 card bg-base-200 p-4">
           <FarmReward farmAddress={farmAddress} />
         </div>
-        <div className="col-span-full @xl/left:col-span-6 card bg-base-200 p-4 ring-2 ring-primary">
+        <div className="col-span-full @xl/left:col-span-6 card bg-base-200 p-4">
           <MyReward farmAddress={farmAddress} />
         </div>
       </div>
