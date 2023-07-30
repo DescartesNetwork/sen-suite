962a019af01f9e6157b45c1789d28794bbd56f9e
tuphan-dn <tuphan@descartes.network>
2023-07-30 17:37:28 +0700

    bugfix

--- src/app/airdrop/bulk-sender/header.tsx
+++ src/app/airdrop/bulk-sender/header.tsx
@@ -1,14 +1,22 @@
 'use client'
-import { useRouter } from 'next/navigation'
+import { usePathname, useRouter } from 'next/navigation'
 
 import { ChevronLeft } from 'lucide-react'
+import { useCallback } from 'react'
 
 export default function BulkSenderHeader() {
-  const { back } = useRouter()
+  const pathname = usePathname()
+  const { push } = useRouter()
+
+  const onBack = useCallback(() => {
+    const hops = pathname.split('/')
+    hops.pop()
+    return push(hops.join('/'))
+  }, [push, pathname])
 
   return (
     <div className="flex flex-row gap-2 items-center">
-      <button className="btn btn-sm btn-circle" onClick={back}>
+      <button className="btn btn-sm btn-circle" onClick={onBack}>
         <ChevronLeft />
       </button>
       <h5 className="">Bulk Sender</h5>
