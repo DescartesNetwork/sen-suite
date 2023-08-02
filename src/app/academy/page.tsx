5ff9f919eb81da0a5bc37cb6de90a9b99dbab06b
tuphan-dn <tuphan@descartes.network>
2023-08-02 18:14:35 +0700

    temp img

--- src/app/academy/page.tsx
+++ src/app/academy/page.tsx
@@ -6,16 +6,20 @@ export default async function Academy() {
   const { pageIds } = await getDatabase()
 
   return (
-    <div className="grid grid-cols-12 gap-4 @container">
-      <h2 className="col-span-full text-center py-8">Sentre Academy</h2>
-      {pageIds.map((pageId) => (
-        <div
-          key={pageId}
-          className="col-span-full @xl:col-span-6 @4xl:col-span-4"
-        >
-          <Thumbnail pageId={pageId} />
+    <div className="flex flex-row justify-center rounded-3xl bg-swap-light dark:bg-swap-dark bg-center bg-cover transition-all p-4">
+      <div className="w-full max-w-[1240px]">
+        <div className="grid grid-cols-12 gap-4 @container">
+          <h2 className="col-span-full text-center py-8">Sentre Academy</h2>
+          {pageIds.map((pageId) => (
+            <div
+              key={pageId}
+              className="col-span-full @xl:col-span-6 @4xl:col-span-4"
+            >
+              <Thumbnail pageId={pageId} />
+            </div>
+          ))}
         </div>
-      ))}
+      </div>
     </div>
   )
 }
--- src/app/academy/thumbnail/index.tsx
+++ src/app/academy/thumbnail/index.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import { getBlockTitle, getPageProperty } from 'notion-utils'
 import dayjs from 'dayjs'
+import { v4 as uuid } from 'uuid'
 
 import { getPage } from '../utils'
 
@@ -12,26 +13,33 @@ export default async function Thumbnail({ pageId }: ThumbnailProps) {
   const map = await getPage(pageId)
   const [{ value: block }] = Object.values(map.block)
 
-  const imgs = ['https://placehold.co/600x400']
   const updatedAt = getPageProperty<number>('Date', block, map) || Date.now()
   const tags = getPageProperty<string[]>('Tags', block, map) || []
   const title = getBlockTitle(block, map)
   const description = getPageProperty<string[]>('Description', block, map) || []
+  const filename =
+    getPageProperty<string>('Files & media', block, map) || uuid()
+  const img =
+    Object.values(map.signed_urls)[1] ||
+    Object.values(map.signed_urls).find((url) =>
+      url.includes(`/${filename}`),
+    ) ||
+    'https://placehold.co/600x400'
 
   return (
     <Link
-      className="card h-full bg-base-100 rounded-box shadow hover:shadow-xl"
+      className="card h-full bg-base-100 rounded-box shadow hover:shadow-lg transition-all"
       href={`/academy/${pageId}`}
     >
       <figure>
-        <img src={imgs[0]} alt={pageId} />
+        <img src={img} alt={pageId} />
       </figure>
       <div className="card-body">
         <p className="text-sm opacity-60">
           {dayjs(updatedAt).format('MMM DD, YYYY')}
         </p>
         <h2 className="card-title">{title}</h2>
-        <p className="opacity-60">{description}</p>
+        <p className="opacity-60 pb-4">{description}</p>
         {tags.map((tag) => (
           <div key={tag} className="badge badge-outline">
             {tag}
