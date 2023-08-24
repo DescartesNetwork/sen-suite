09602dc4b71a61f7a750d123658c3ebe0233b45b
tuanle161099 <tuanle@descartes.network>
2023-08-24 16:32:14 +0700

    boost NFT

--- src/app/farming/farm-details/boostInfo.tsx
+++ src/app/farming/farm-details/boostInfo.tsx
@@ -1,11 +1,18 @@
-import { MintAmount } from '@/components/mint'
-import { precision } from '@/hooks/farming.hook'
+import { useMemo } from 'react'
+import BN from 'bn.js'
+
+import { MintAmount, MintSymbol } from '@/components/mint'
+
+import { precision, useCollectionBoosted } from '@/hooks/farming.hook'
 import {
+  useBoostingByFarmAddress,
   useDebtByFarmAddress,
   useFarmByAddress,
 } from '@/providers/farming.provider'
-import BN from 'bn.js'
-import { useMemo } from 'react'
+import { useNfts } from '@/hooks/mpl.hook'
+import { numeric } from '@/helpers/utils'
+import { decimalize, undecimalize } from '@/helpers/decimals'
+import { useMintByAddress } from '@/providers/mint.provider'
 
 type BoostInfoType = {
   farmAddress: string
@@ -16,7 +23,13 @@ type BoostInfoType = {
 const BoostInfo = ({ farmAddress, amount, nfts }: BoostInfoType) => {
   const { inputMint } = useFarmByAddress(farmAddress)
   const debt = useDebtByFarmAddress(farmAddress)
-
+  const boosting = useBoostingByFarmAddress(farmAddress)
+  const { decimals } = useMintByAddress(inputMint.toBase58()) || { decimals: 0 }
+  const nftsMetadata = useNfts(nfts)
+  const collections = nftsMetadata.map((metadata) =>
+    metadata?.collection?.address.toBase58(),
+  )
+  const collectionBoosted = useCollectionBoosted(farmAddress)
   const stakedAmount = useMemo(
     () =>
       !debt || (!amount && !nfts.length)
@@ -24,6 +37,38 @@ const BoostInfo = ({ farmAddress, amount, nfts }: BoostInfoType) => {
         : debt.shares.mul(precision).div(debt.leverage),
     [amount, debt, nfts.length],
   )
+
+  const { boostRate, totalBoosted } = useMemo(() => {
+    let boostRate = new BN(0)
+    let totalBoosted = new BN(0)
+    if (!nfts.length) return { boostRate, totalBoosted }
+
+    for (const { boostingCollection, boostingCoefficient } of boosting) {
+      if (collections.includes(boostingCollection.toBase58()))
+        boostRate = boostRate.add(boostingCoefficient)
+      if (collectionBoosted.includes(boostingCollection.toBase58()))
+        totalBoosted = totalBoosted.add(boostingCoefficient)
+    }
+
+    return { boostRate, totalBoosted }
+  }, [boosting, collectionBoosted, collections, nfts.length])
+  const displayBoosted = !amount && !nfts.length ? new BN(0) : totalBoosted
+  const totalBoostRate = displayBoosted.add(boostRate)
+
+  const boostByNFT = useMemo(() => {
+    if (!amount) return new BN(0)
+    return stakedAmount
+      .add(decimalize(amount.toString(), decimals))
+      .mul(totalBoostRate)
+      .div(precision)
+  }, [amount, decimals, stakedAmount, totalBoostRate])
+
+  const totalAmountIn = useMemo(
+    () =>
+      decimalize(amount.toString(), decimals).add(stakedAmount).add(boostByNFT),
+    [amount, boostByNFT, decimals, stakedAmount],
+  )
+
   return (
     <div className="card p-4 bg-base-300 mb-4 gap-2">
       <div className="flex justify-between items-center">
@@ -32,19 +77,25 @@ const BoostInfo = ({ farmAddress, amount, nfts }: BoostInfoType) => {
       </div>
       <div className="flex justify-between items-center">
         <p className="text-sm opacity-60">LP amount</p>
-        <p>23</p>
+        <p>
+          {amount} <MintSymbol mintAddress={inputMint.toBase58()} />
+        </p>
       </div>
       <div className="flex justify-between items-center">
         <p className="text-sm opacity-60">Total boost rate</p>
-        <p>23</p>
+        <p> {numeric(undecimalize(totalBoostRate, 9)).format('0,0.[00]%')}</p>
       </div>
       <div className="flex justify-between items-center">
         <p className="text-sm opacity-60"> Boosted by NFT</p>
-        <p>23</p>
+        <p>
+          {numeric(undecimalize(boostByNFT, decimals)).format('0,0.[0000]')}
+        </p>
       </div>
       <div className="flex justify-between items-center">
         <p className="text-sm opacity-60"> Total</p>
-        <p>23</p>
+        <p>
+          {numeric(undecimalize(totalAmountIn, decimals)).format('0,0.[0000]')}
+        </p>
       </div>
       <p className="text-xs opacity-60">
         *Total = Staked LP + LP amount + Boosted LP
