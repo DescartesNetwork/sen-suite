aae5a18899dfe2cc67e59d9c814e278266d657cc
tuphan-dn <tuphan@descartes.network>
2023-08-29 10:57:13 +0700

    uifix

--- src/app/farming/layout.tsx
+++ src/app/farming/layout.tsx
@@ -13,9 +13,7 @@ export default function FarmingLayout({ children }: { children: ReactNode }) {
   return (
     <div className="flex flex-col h-full rounded-3xl bg-swap-light dark:bg-swap-dark bg-center bg-cover transition-all p-4 gap-4 items-center">
       <FarmingProvider>
-        <div className="max-w-[1024px] w-full grid grid-cols-12 gap-x-2 gap-y-4">
-          <div className="col-span-full">{children}</div>
-        </div>
+        <div className="max-w-[1024px] w-full h-full">{children}</div>
 
         {/* Services worker */}
         <FarmingWatcher />
