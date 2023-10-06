9ff8d1ebcfcf4a9e6e110461487d00fccb8cc092
tuphan-dn <tuphan@descartes.network>
2023-10-06 10:40:09 +0700

    fix: add meta description

--- src/app/welcome/description.tsx
+++ src/app/welcome/description.tsx
@@ -3,12 +3,12 @@
 export default function Description() {
   return (
     <div className="list-app relative">
-      <div className="sticky pos-center top-0 left-0 h-[100vh] md:h-[120vh] w-full gap-10 bg-center bg-no-repeat bg-cover bg-[url('/apps-bg-light.png')] dark:bg-[url('/apps-bg-dark.png')]">
-        <h5>
+      <div className="sticky pos-center top-0 left-0 h-[100dvh] w-full gap-10 bg-center bg-no-repeat bg-cover bg-[url('/apps-bg-light.png')] dark:bg-[url('/apps-bg-dark.png')] p-4">
+        <h3 className="w-full max-w-[1024px]">
           Unlock your project&apos;s full potential with Sentre—Offering swap,
           airdrop tool, launchpad, and more. Begin scaling your decentralized
           journey with us today.
-        </h5>
+        </h3>
       </div>
       <div className="h-[100vh] w-full" />
     </div>
