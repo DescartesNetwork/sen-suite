e228b9c85c14a6d3b944d953b34f26b89da2463a
quanhoang882002 <135295387+hnquan8@users.noreply.github.com>
2023-11-29 20:14:40 +0700

    refactor code animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="home-page w-full h-full rounded-3xl bg-cover bg-swap-light dark:bg-swap-dark">
       <Welcome />
-      <div id="isHidden" className="pos-center">
+      <div id="btn-scroll" className="pos-center">
         <ScrollButton />
       </div>
     </div>
