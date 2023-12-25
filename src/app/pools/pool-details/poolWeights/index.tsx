4e74016b9f5d1cd41dd1e16b45c276faa0de0145
tuphan-dn <tuphan@descartes.network>
2023-12-25 15:47:45 +0700

    fix: migrate echarts to recharts

--- src/app/pools/pool-details/poolWeights/index.tsx
+++ src/app/pools/pool-details/poolWeights/index.tsx
@@ -1,12 +1,31 @@
 'use client'
-import { useMemo } from 'react'
+import { Fragment, useMemo } from 'react'
 
-import DoughnutChart from './doughnutChart'
+import {
+  Cell,
+  Legend,
+  Pie,
+  PieChart,
+  ResponsiveContainer,
+  Tooltip,
+} from 'recharts'
 
 import { usePoolByAddress } from '@/providers/pools.provider'
 import { undecimalize } from '@/helpers/decimals'
 import { useOracles } from '@/hooks/pool.hook'
 import { useMintStore } from '@/providers/mint.provider'
+import { numeric } from '@/helpers/utils'
+
+const COLORS = [
+  '#ff6961',
+  '#ffb480',
+  '#f8f38d',
+  '#42d6a4',
+  '#08cad1',
+  '#59adf6',
+  '#9d94ff',
+  '#c780e8',
+]
 
 export default function PoolWeights({ poolAddress }: { poolAddress: string }) {
   const poolData = usePoolByAddress(poolAddress)
@@ -24,17 +43,54 @@ export default function PoolWeights({ poolAddress }: { poolAddress: string }) {
       return {
         symbol,
         tokenAmount: undecimalize(reserve, decimals) || '0',
-        weight: normalizedWeight * 100,
+        weight: normalizedWeight,
       }
     })
     return newData
   }, [getMintInfo, metadata, poolData])
 
   return (
-    <div className="card rounded-3xl p-6 bg-base-100 border border-base-300 flex flex-col ">
+    <div className="card rounded-box p-6 bg-base-100 border border-base-300 flex flex-col ">
       <p>Pool Weights</p>
       <div className="flex items-center justify-center h-[306px]">
-        <DoughnutChart data={poolWeights} />
+        <ResponsiveContainer className="w-full h-full">
+          <PieChart>
+            <Pie
+              data={poolWeights}
+              className="cursor-pointer"
+              dataKey="weight"
+              nameKey="symbol"
+              fill="#8884d8"
+              label
+            >
+              {poolWeights.map(({ symbol }, i) => (
+                <Cell key={symbol} fill={COLORS[i]} />
+              ))}
+            </Pie>
+            <Tooltip
+              content={({ active, payload }) => {
+                if (!active || !payload || !payload.length) return <Fragment />
+                const [
+                  {
+                    payload: { symbol, tokenAmount, weight },
+                  },
+                ] = payload
+                return (
+                  <div className="card rounded-box p-4 bg-base-100 border-2 border-base-300 flex flex-col">
+                    <p>{symbol}</p>
+                    <p className="font-bold">
+                      {numeric(tokenAmount).format('$0,0.[0000]')}
+                    </p>
+                    <p className="font-bold">
+                      {numeric(weight).format('%0.[0000]')}
+                    </p>
+                  </div>
+                )
+              }}
+            />
+            <Legend />
+          </PieChart>
+        </ResponsiveContainer>
       </div>
     </div>
   )
