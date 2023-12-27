55d3ba3f4df56df6c9ff3daba75662f61131102b
tuphan-dn <tuphan@descartes.network>
2023-12-27 15:08:35 +0700

    fix: fix bulksender

--- src/app/token-distribution/bulk-sender/summary/page.tsx
+++ src/app/token-distribution/bulk-sender/summary/page.tsx
@@ -4,7 +4,6 @@ import Link from 'next/link'
 import BN from 'bn.js'
 import { redirect } from 'next/navigation'
 
-import { ChevronLeft } from 'lucide-react'
 import { MintLogo, MintSymbol } from '@/components/mint'
 import EditRowBulkSender from './row'
 
@@ -31,6 +30,14 @@ enum RowStatus {
 
 const TX_SIZE = 7
 
+function safeParseBN(a: string) {
+  try {
+    return new BN(a)
+  } catch (er) {
+    return new BN(0)
+  }
+}
+
 export default function SummaryBulkSender() {
   const [loading, setLoading] = useState(false)
   const [newAmount, setNewAmount] = useState('')
@@ -46,12 +53,12 @@ export default function SummaryBulkSender() {
   const decimals = useMemo(() => mint?.decimals, [mint?.decimals])
   const amount = useMemo(
     () =>
-      data.reduce(
-        (s, [_, a]) =>
-          decimals === undefined ? new BN(0) : decimalize(a, decimals).add(s),
-        new BN(0),
-      ),
-    [data, decimals],
+      data.reduce((s, [_, a]) => {
+        if (decimals === undefined) return new BN(0)
+        if (decimalized) return s.add(safeParseBN(a))
+        return decimalize(a, decimals).add(s)
+      }, new BN(0)),
+    [data, decimals, decimalized],
   )
   const tvl = useTvl([{ mintAddress, amount }])
 
@@ -61,6 +68,7 @@ export default function SummaryBulkSender() {
         if (!isAddress(address)) return RowStatus.BadAddress
         if (Number(amount) < 0) return RowStatus.BadAmount
         if (Number(amount) === 0) return RowStatus.ZeroAmount
+        if (decimalized && Number(amount) % 1 !== 0) return RowStatus.BadAmount
         if (
           data
             .map(([next], j) => next === address && i !== j)
@@ -69,7 +77,7 @@ export default function SummaryBulkSender() {
           return RowStatus.Duplicated
         return RowStatus.Good
       }),
-    [data],
+    [data, decimalized],
   )
   const errors = useMemo(
     () =>
@@ -174,127 +182,122 @@ export default function SummaryBulkSender() {
   if (!isAddress(mintAddress))
     return redirect('/token-distribution/bulk-sender')
   return (
-    <div className="grid grid-cols-12 gap-4">
-      <div className="col-span-full">
-        <Link
-          href={'/token-distribution/bulk-sender'}
-          className="btn btn-sm btn-ghost "
-        >
-          <ChevronLeft size={16} />
-          Back
-        </Link>
-      </div>
-      <div className="col-span-full card bg-base-100 rounded-3xl p-6 shadow-xl grid grid-cols-12 gap-8 ">
-        <h5 className="col-span-full">Bulk Sender</h5>
-        <div className="col-span-full grid grid-cols-12 gap-x-2 gap-y-6">
-          <div className="col-span-full card flex flex-row gap-2 items-center">
-            <MintLogo
-              className="h-9 w-9 rounded-full"
-              mintAddress={mintAddress}
+    <div className="col-span-full card bg-base-100 rounded-3xl p-6 shadow-xl grid grid-cols-12 gap-y-8 gap-x-2">
+      <h5 className="col-span-full">Bulk Sender</h5>
+      <div className="col-span-full grid grid-cols-12 gap-x-2 gap-y-6">
+        <div className="col-span-full card flex flex-row gap-2 items-center">
+          <MintLogo
+            className="h-9 w-9 rounded-full"
+            mintAddress={mintAddress}
+          />
+          <p className="font-bold flex-auto">
+            <MintSymbol mintAddress={mintAddress} />
+          </p>
+          <label className="label cursor-pointer gap-2">
+            <span className="label-text">Enable decimals</span>
+            <input
+              type="checkbox"
+              className="checkbox"
+              checked={decimalized}
+              onChange={(e) => setDecimalized(e.target.checked)}
             />
-            <p className="font-bold flex-auto">
-              <MintSymbol mintAddress={mintAddress} />
-            </p>
-            <label className="label cursor-pointer gap-2">
-              <span className="label-text">Enable decimals</span>
-              <input
-                type="checkbox"
-                className="checkbox"
-                checked={decimalized}
-                onChange={(e) => setDecimalized(e.target.checked)}
-              />
-            </label>
-          </div>
-          <div className="col-span-full flex flex-col gap-2">
-            {data.map(([address, amount], i) => (
-              <EditRowBulkSender
-                key={`${address}-${i}`}
-                index={String(i + 1)}
-                address={address}
-                amount={amount}
-                onClick={() => onDelete(i)}
-                error={
-                  statuses[i] === RowStatus.BadAddress ||
-                  statuses[i] === RowStatus.BadAmount
-                }
-                warning={
-                  statuses[i] === RowStatus.Duplicated ||
-                  statuses[i] === RowStatus.ZeroAmount
-                }
-              />
-            ))}
+          </label>
+        </div>
+        <div className="col-span-full flex flex-col gap-2">
+          {data.map(([address, amount], i) => (
             <EditRowBulkSender
-              index={String(data.length + 1)}
-              address={newAddress}
-              onAddress={setNewAddress}
-              amount={newAmount}
-              onAmount={setNewAmount}
-              onClick={onAdd}
-              toAdd
+              key={`${address}-${i}`}
+              index={String(i + 1)}
+              address={address}
+              amount={amount}
+              onClick={() => onDelete(i)}
+              error={
+                statuses[i] === RowStatus.BadAddress ||
+                statuses[i] === RowStatus.BadAmount
+              }
+              warning={
+                statuses[i] === RowStatus.Duplicated ||
+                statuses[i] === RowStatus.ZeroAmount
+              }
             />
-          </div>
-          <div className="col-span-full  @container">
-            <div className="flex flex-row justify-between items-center mb-6">
-              {errors > 0 && (
-                <span className="text-error text-sm font-semibold">{`${errors} error(s)`}</span>
-              )}
-              {warnings > 0 && (
-                <span className="text-warning text-sm font-semibold">{`${warnings} warning(s)`}</span>
-              )}
-              {!errors && !warnings && (
-                <span className="text-success text-sm font-semibold">
-                  Optimized
-                </span>
-              )}
-              <div className="flex flex-row gap-2 items-center">
-                <button
-                  className="btn btn-xs btn-outline rounded-full btn-error"
-                  onClick={onFilterZeros}
-                  disabled={!statuses.find((e) => e === RowStatus.ZeroAmount)}
-                >
-                  Remove zeros
-                </button>
+          ))}
+          <EditRowBulkSender
+            index={String(data.length + 1)}
+            address={newAddress}
+            onAddress={setNewAddress}
+            amount={newAmount}
+            onAmount={setNewAmount}
+            onClick={onAdd}
+            toAdd
+          />
+        </div>
+        <div className="col-span-full  @container">
+          <div className="flex flex-row justify-between items-center mb-6">
+            {errors > 0 && (
+              <span className="text-error text-sm font-semibold">{`${errors} error(s)`}</span>
+            )}
+            {warnings > 0 && (
+              <span className="text-warning text-sm font-semibold">{`${warnings} warning(s)`}</span>
+            )}
+            {!errors && !warnings && (
+              <span className="text-success text-sm font-semibold">
+                Optimized
+              </span>
+            )}
+            <div className="flex flex-row gap-2 items-center">
+              <button
+                className="btn btn-xs btn-outline rounded-full btn-error"
+                onClick={onFilterZeros}
+                disabled={!statuses.find((e) => e === RowStatus.ZeroAmount)}
+              >
+                Remove zeros
+              </button>
 
-                <button
-                  className="btn btn-xs btn-outline rounded-full btn-error"
-                  onClick={onMergeDuplicates}
-                  disabled={!statuses.find((e) => e === RowStatus.Duplicated)}
-                >
-                  Merge duplicates
-                </button>
-              </div>
+              <button
+                className="btn btn-xs btn-outline rounded-full btn-error"
+                onClick={onMergeDuplicates}
+                disabled={!statuses.find((e) => e === RowStatus.Duplicated)}
+              >
+                Merge duplicates
+              </button>
             </div>
-            <div className="card flex flex-col bg-base-200 rounded-box w-full p-4 gap-2">
-              <div className=" flex flex-row justify-between">
-                <p className="text-sm opacity-60">Receivers</p>
-                {numeric(data.length).format('0,0')}
-              </div>
-              <div className="flex flex-row justify-between">
-                <p className="text-sm opacity-60">Total value</p>
-                <div className="flex flex-col gap-1">
-                  <div className="flex flex-row text-xl font-bold">
-                    {numeric(undecimalize(amount, mint?.decimals || 0)).format(
-                      '0,0.[0000]',
-                    )}{' '}
-                    <MintSymbol mintAddress={mintAddress} />
-                  </div>
-                  <p className="text-right opacity-60">
-                    {numeric(tvl).format('$0a.[0000]')}
-                  </p>
+          </div>
+          <div className="card flex flex-col bg-base-200 rounded-box w-full p-4 gap-2">
+            <div className=" flex flex-row justify-between">
+              <p className="text-sm opacity-60">Receivers</p>
+              {numeric(data.length).format('0,0')}
+            </div>
+            <div className="flex flex-row justify-between">
+              <p className="text-sm opacity-60">Total value</p>
+              <div className="flex flex-col gap-1">
+                <div className="flex flex-row text-xl font-bold">
+                  {numeric(undecimalize(amount, mint?.decimals || 0)).format(
+                    '0,0.[0000]',
+                  )}{' '}
+                  <MintSymbol mintAddress={mintAddress} />
                 </div>
+                <p className="text-right opacity-60">
+                  {numeric(tvl).format('$0a.[0000]')}
+                </p>
               </div>
             </div>
           </div>
         </div>
-        <button
-          className="col-span-full btn btn-primary rounded-3xl"
-          onClick={onSend}
-          disabled={loading || !data.length}
-        >
-          {loading && <span className="loading loading-spinner loading-sm" />}
-          {isRetry ? 'Retry' : 'Send'}
-        </button>
       </div>
+      <Link
+        href={'/token-distribution/bulk-sender'}
+        className="col-span-6 btn btn-ghost rounded-full"
+      >
+        Back
+      </Link>
+      <button
+        className="col-span-6 btn btn-primary rounded-full"
+        onClick={onSend}
+        disabled={loading || !data.length}
+      >
+        {loading && <span className="loading loading-spinner loading-sm" />}
+        {isRetry ? 'Retry' : 'Send'}
+      </button>
     </div>
   )
 }
